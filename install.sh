66f523eec6e8c3e3dc68f216f06bea6c129a82f3
arai <>
2020-06-01 18:48:20 +0900

    リンクがうまく貼れなかったのでスクリプトの中身を変更　成功

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
-#...
+#!/bin/bash
 
-DOTPATH=~/.dotfiles
+set -u
 
-for f in .??*
-do
+# 実行場所のディレクトリを取得
+THIS_DIR=$(cd $(dirname $0); pwd)
+
+cd $THIS_DIR
+git submodule init
+git submodule update
+
+echo "start setup..."
+for f in .??*; do
     [ "$f" = ".git" ] && continue
+    [ "$f" = ".gitconfig.local.template" ] && continue
+    [ "$f" = ".require_oh-my-zsh" ] && continue
+    [ "$f" = ".gitmodules" ] && continue
 
-    ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
-  done
-
+    ln -snfv ~/dotfiles/"$f" ~/
+done
